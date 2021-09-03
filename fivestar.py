b4e2d9458187e52e7593c965608f8565b219033e
Sylvia Hela Mullins <49255017+Helasrebellion@users.noreply.github.com>
2021-09-03 19:45:02 -0400

    Create fivestar.py

---
+++ fivestar.py
@@ -0,0 +1,8 @@
+newVideos = float(input("Enter the number of new videos:"))
+oldieS = float(input("Enter the number of oldies:"))
+
+newCost = newVideos * 3
+oldieCost = oldieS * 2
+
+finalCost = newCost + oldieCost
+print("The total cost is: ", finalCost)
